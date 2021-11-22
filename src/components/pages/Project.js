891be466d45d11a6c3186206129d06ba779dc510
Zeyang Lai <decyctus@gmail.com>
2021-11-21 22:04:57 -0800

    add more projects

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
+import Project1 from "../../img/project1.png";
+import Budget from "../../img/budget.png";
+import Employee from "../../img/employee.png";
+import Magic from "../../img/magic.png";
+import Weather from "../../img/weather.png";
+
 
 export default function Project() {
   return (
-    <div>
+    <div className="make-neat">
       <h1>Project Page</h1>
-      <p>
-          Projects show in here!
-      </p>
+      <div className="">
+        <img src={Project1} className="card-img" alt="..." />
+      </div>
+      <div className="">
+        <img src={Budget} className="card-img" alt="..." />
+      </div>
+      <div className="">
+        <img src={Employee} className="card-img" alt="..." />
+      </div>
+      <div className="">
+        <img src={Magic} className="card-img" alt="..." />
+      </div>
+      <div className="">
+        <img src={Weather} className="card-img" alt="..." />
+      </div>
     </div>
   );
 }
